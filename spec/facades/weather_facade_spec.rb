e8f683edb2ea72d9d541309a892e13889c7a19b1
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-05-15 12:44:50 -0600

    finish tests for weather facade for now

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
+require './app/services/map_quest_service'
+require './app/facades/weather_facade'
 
 RSpec.describe WeatherFacade do
   it '.forecast' do
     VCR.use_cassette('jacksonville,fl') do
       forecast = WeatherFacade.forecast('jacksonville,fl')
+
+      expect(forecast).to have_key(:lat)
+      expect(forecast).to have_key(:lng)
     end
   end
 end
