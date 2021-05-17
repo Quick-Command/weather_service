69be7005e91d188f335c1e5de0a74baff93b4156
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-05-17 13:24:45 -0600

    add coverage to git ignore file

---
+++ app/facades/weather_facade.rb
@@ -0,0 +1,14 @@
+class WeatherFacade
+  def self.forecast(location)
+    location_data = MapQuestService.call(location)
+    coordinates = lookup(location)
+    #weather_data = WeatherService.call(coordinates[:lat], coordinates[:lng])
+    #ForecastPoro.new(weather_data)
+  end
+
+  def self.lookup(location)
+   location_data = MapQuestService.call(location)
+   coordinates = location_data[:results][0][:locations][0][:latLng]
+   coordinates if coordinates != { lat: 39.390897, lng: -99.066067}
+ end
+end
